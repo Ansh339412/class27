fb318b6ff13af022f79fc498f53cb2300d1e9445
Ansh339412 <hakikatsingh099@gmail.com>
2020-10-02 18:37:56 +0530

    created canstraint

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
-
+const Constraint=Matter.Constraint;
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
@@ -35,7 +35,8 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
-
+     log6=new Log(230,180,80,PI/2)
+     chain=new Chain(bird.body,log6.body)
 }
 
 function draw(){
@@ -61,4 +62,6 @@ function draw(){
 
     bird.display();
     platform.display();
+    chain.display()
+    log6.display()
 }
